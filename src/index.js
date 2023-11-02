4466249efa8ee76b5aaa8927d0bd5d6fec3a1da4
Oleksandr Bry <134804799+Prybluda@users.noreply.github.com>
2023-11-02 21:16:57 +0100

    111

---
+++ src/index.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+import Notiflix, { Notify } from "notiflix";
+
+const input = document.querySelector('input[name="searchQuery"]');
+// const subBtn = document.querySelector('button[type="submit"]');
+const form = document.querySelector('.search-form');
+const gallery =document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
+
+let page = 1;
+let per_page = 40;
+
+btnLoadMore.style.display = 'none';
+
+async function getApi(searchImg, page, perPage) {
+    const URL = 'https://pixabay.com/api/';
+    const API_KEY = "40376854-8dce2a421219bab32f4b26962";
+
+    try {
+      const response = await axios.get(
+        `${URL}?key=${API_KEY}&q=${searchImg}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+        // console.log(response.data);
+      return response.data;
+      
+    } catch (error) {
+        // console.log(error)
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    }
+    
+  }
+  getApi()
+ 
+input.addEventListener('submit', pressSearch);
+
+async function pressSearch(e){
+    e.preventDefault();
+    const searchImg = input.value;
+    input.value = '';
+    page = 1;
+    gallery.innerHTML= '';
+    try {
+        const data = await getApi(searchImg, page, per_page);
+        const images = data.hits;
+        const markup = createMarkup(images);
+        gallery.insertAdjacentHTML('beforeend', markup);
+        if(images.length === 0) {
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        } else if (images.length < per_page){
+            btnLoadMore.style.display = 'none';
+        } else {
+            btnLoadMore.style.display = 'block';
+        }
+
+    }catch (error) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
+    }
+}
+ function createMarkup(images){
+    return images.map(
+        ({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>
+        `<div class="photo-card">
+        <a href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes:${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views:${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments:${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads:${downloads}</b>
+      </p>
+    </div>
+  </div>`).join('');
+}
+
+
+async function loadMore() {
+    page += 1;
+    const searchImg = input.value;
+  
+    try {
+      const data = await getApi(searchImg, page, per_page);
+      const images = data.hits;
+      const markup = createMarkup(images);
+      gallery.insertAdjacentHTML('beforeend', markup);
+      if (images.length < per_page) {
+        btnLoadMore.style.display = 'none';
+      }
+    } catch (error) {
+      // Обробка помилок API-запиту
+    }
+  }
+  
+  btnLoadMore.addEventListener('click', loadMore);
+  form.addEventListener('submit', pressSearch);
+
